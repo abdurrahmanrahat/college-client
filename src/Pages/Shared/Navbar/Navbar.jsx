583a04555480fd1110ed86bf65030cab832e55e8
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-07-22 11:26:11 +0600

    Navbar design done

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,66 @@
+import ActiveLink from "../../../components/ActiveLink/ActiveLink";
+import Logo from "../../../assets/bookLogo-notBg.png";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h2>Navbar</h2>
+  const navLinks = (
+    <>
+      <li>
+        <ActiveLink to="/">Home</ActiveLink>
+      </li>
+      <li>
+        <ActiveLink to="/colleges">Colleges</ActiveLink>
+      </li>
+      <li>
+        <ActiveLink to="/admission">Admission</ActiveLink>
+      </li>
+      <li>
+        <ActiveLink to="/myCollege">My College</ActiveLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-3 shadow bg-base-100 rounded-box w-36"
+          >
+            {navLinks}
+          </ul>
         </div>
-    );
+        <div className="flex">
+          <img src={Logo} className="w-40 rounded ml-6 md:ml-0" alt="" />
+          <h2 className="md:hidden text-xl font-bold mt-2 ml-8">College</h2>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="md:flex justify-center items-center font-medium uppercase">
+          {navLinks}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
